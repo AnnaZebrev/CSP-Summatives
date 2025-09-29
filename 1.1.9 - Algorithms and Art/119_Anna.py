09864c36eff2871e6b8fc96724f05f80ea44ffb2
AnnaZebrev <312544@shakopeeschools.org>
2025-09-29 11:17:50 -0500

    Update 119_Anna.py
    I imported turtle and started creating my custom shape. I also asked the user for input to change the color of the shape.

---
+++ 1.1.9 - Algorithms and Art/119_Anna.py
@@ -0,0 +1,12 @@
+import turtle as trtl
+
+#create a heart shaped turtle
+trtl.addshape("Heart", ((0,5), (2,7), (3,8), (5,9), (6,9), (7,9), ))
+
+#welcome users and aintodouce them to the activity
+print("Welcome user! Let's draw a picture!")
+
+#pick a heart color
+heart_color = input("What color would you like the heart to be?")
+
+
