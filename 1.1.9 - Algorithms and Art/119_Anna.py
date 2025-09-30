bd58fc72e99ca27f61bd3be50df9072c7f3efb28
AnnaZebrev <312544@shakopeeschools.org>
2025-09-30 10:17:28 -0500

    Update 119_Anna.py

--- 1.1.9 - Algorithms and Art/119_Anna.py
+++ 1.1.9 - Algorithms and Art/119_Anna.py
@@ -1,7 +1,7 @@
 import turtle as trtl
 
 #create a heart shaped turtle
-trtl.addshape("Heart", ((0,5), (2,7), (3,8), (5,9), (6,9), (7,9), ))
+trtl.addshape("heart", ((0,5), (2,7), (3,8), (5,9), (6,9), (7,9), (8,8.5), (9,8), (10,6.5), (11,4), (10.5,2), (9.5,0), (0, -11), (-9.5,0), (-10.5,2), (-11,4), (-10,6.5), (-9,8), (-8,8.5), (-7,9), (-6,9), (-5,9), (-3,8), (-2,7)))
 
 #welcome users and aintodouce them to the activity
 print("Welcome user! Let's draw a picture!")
@@ -9,4 +9,14 @@ print("Welcome user! Let's draw a picture!")
 #pick a heart color
 heart_color = input("What color would you like the heart to be?")
 
+#create heart turtle
+heart = trtl.Turtle(shape="heart")
 
+#draw a heart in the center of the screen
+heart.color(heart_color)
+heart.setheading(90)
+heart.turtlesize(3)
+
+#keep screen after program runs
+wn = trtl.Screen()
+wn.mainloop()
