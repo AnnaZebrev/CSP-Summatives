62725a7433a19780357be5b7d1eb873808e615f8
AnnaZebrev <312544@shakopeeschools.org>
2025-10-29 11:16:41 -0500

    Fixed start problem and changed start procedure
    Added turtles to click to start instead of asking for user input

--- 1.2.5 - Shall We Play a Game/1.2.5_Whac_A_Mole_summative.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_Whac_A_Mole_summative.py
@@ -1,16 +1,46 @@
-import turtle as turtle
+import turtle as trtl
 import random
-import leaderboard
+#import leaderboard
+
+play = False
 
 #Start Screen
 
-    #Ask if user wants to play
+#Ask if user wants to play
+
+yes_turtle = trtl.Turtle(shape="turtle")
+yes_turtle.turtlesize(4)
+yes_turtle.teleport(-60,0)
+
+no_turtle = trtl.Turtle(shape="turtle")
+no_turtle.turtlesize(4)
+no_turtle.teleport(60,0)
+
+
+'''
+start = trtl.textinput("Would you like to play?", "y/n?")
+start = start.lower()
+while start != "y" and start != "n":
+    start = trtl.textinput("Please type y or n", "y/n?")
+if start == "y":
+    play = True #while loop for game play
+if start == "n":
+    trtl.hideturtle()
+    trtl.write("Goodbye!")
+'''
+
+while play == True:
 
     #Ask user name
+    player_name = trtl.textinput("Name", "Enter your name")
+
+    while "," in player_name or len(player_name)==0:
+        player_name = trtl.textinput("Name", "Please do not use a comma, Enter your name")
 
 #Add background and turtle images
 
     #Main background
+
 
     #Create turtle image instead of shape
 
@@ -41,3 +71,5 @@ import leaderboard
 
     #Restart loop or say goodbye
 
+wn = trtl.Screen()
+wn.mainloop()
