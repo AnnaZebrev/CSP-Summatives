c47ec68e6ef13c5c286c9d752da733d9664c8deb
AnnaZebrev <312544@shakopeeschools.org>
2025-10-31 10:34:52 -0500

    fixed problems and worked on randomization
    fixed the starting problem and made a leaderboard but I have to fix it, and I started woking on randomization for the turtle

--- 1.2.5 - Shall We Play a Game/1.2.5_Whac_A_Mole_summative.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_Whac_A_Mole_summative.py
@@ -1,18 +1,17 @@
 import turtle as trtl
-import random
-#import leaderboard
+import random as rand
+#import leaderboard as lb
 
-font_setup = ("Arial",26,"normal")
-   
+font_setup = ("Arial",40,"normal")
+score = 0
 
-play = False
 wn = trtl.Screen()
 
 #Start Screen
 
 #Tell the user how to start
 trtl.hideturtle()
-trtl.teleport(-45,45)
+trtl.teleport(-65,45)
 trtl.write("Click the turtle to play!" , font=font_setup)
 
 play_turtle = trtl.Turtle(shape="turtle")
@@ -25,10 +24,16 @@ def start_click(x,y):
     global play
     #Ask user name
     player_name = trtl.textinput("Name", "Enter your name")
-    play = True
+    
 
     while "," in player_name or len(player_name)==0:
         player_name = trtl.textinput("Name", "Please do not use a comma, Enter your name")
+    start()
+    #create score
+    score_writer = trtl.Turtle()
+    score_writer.hideturtle()
+    score_writer.teleport(-18,165)
+    score_writer.write(score, font=font_setup)
 
 #create leaderboard
 leaderboard_file_name = "125_leaderboard.txt"
@@ -38,7 +43,8 @@ leader_scores_list = []
 #start game
 play_turtle.onclick(start_click)
 
-while play == True:
+
+def start():
 #Add background and turtle images
 
 #screen setup
@@ -56,6 +62,18 @@ while play == True:
 #Add randomization to turtle movement
 
     #Make list of specific locations based on background and then pick a random location from the list
+    mole_xpositions = [0, -210, 210, 0, -210, 210]
+    mole_ypositions = [100, -60, -60, -240, 270, 270]
+    #mole.teleport(0,100)
+    #mole.teleport(-210,-60)
+    #mole.teleport(210,-60)
+    #mole.teleport(0,-240)
+    #mole.teleport(-200, 270)
+    #mole.teleport(210, 270)
+    xcor = rand.choice(mole_xpositions)
+    ycor = mole_ypositions(mole_xpositions[xcor])
+    mole.teleport(xcor, ycor)
+
 
 #Timer
     #Add timer for whole game
@@ -65,9 +83,26 @@ while play == True:
 #Score
 
     #Add/Subtract
+def change_score():
+    score_writer.clear()
+    global score
+    score += 1
+    score_writer.pendown()
+    score_writer.write(score, font=font_setup)
+'''
+#Update leaderboard
+    # get the names and scores from the leaderboard file
+leader_names_list = lb.get_names(leaderboard_file_name)
+leader_scores_list = lb.get_scores(leaderboard_file_name)
 
-    #Update leaderboard
+  # show the leaderboard with or without the current player
+if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
 
+else:
+    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
+'''
     #Placement
 
     #Create medals to be earned for high enough scores **Optional**
